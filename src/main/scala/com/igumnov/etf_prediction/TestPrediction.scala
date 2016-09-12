96498bea05bb8a91590a03538b3ab4aee05c01b0
Evgeny Igumnov <igumnov@gmail.com>
2016-09-12 12:31:11 +0600

    real result

--- src/main/scala/com/igumnov/etf_prediction/TestPrediction.scala
+++ src/main/scala/com/igumnov/etf_prediction/TestPrediction.scala
@@ -20,6 +20,8 @@ package com.igumnov.etf_prediction
 
 
 // $example on$
+import java.nio.file.{Paths, Files}
+
 import org.apache.spark.ml.classification.MultilayerPerceptronClassifier
 import org.apache.spark.ml.evaluation.MulticlassClassificationEvaluator
 
@@ -32,6 +34,42 @@ import org.apache.spark.sql.SparkSession
 object TestPrediction {
 
   def main(args: Array[String]): Unit = {
+
+    import scala.io.Source
+
+    val linesCsv = Source.fromFile("data/spy.csv").getLines()
+    val linesCsvOrdered = linesCsv.toList.reverse
+    val lines17 = tail17(linesCsvOrdered)
+    val rates17 = lines17.map(set =>
+      set.map(line =>
+        (line.split(" ").apply(1)).toDouble
+      )
+    )
+    //    val rates17normal = rates17.map(normal(_))
+
+    val rates14learn = rates17.map(set => {
+      val last4 = normal(set).takeRight(2)
+      val teach = if (last4.head < last4.last) {
+        if ((last4.last - last4.head) > 0.5) 1 else 0
+      }
+      else 0
+      List(teach) ++ normal(set.take(5))
+    })
+
+
+    val lines = prepareLines(rates14learn.dropRight(100))
+
+    Files.write(Paths.get("data/spy.txt"), lines.flatMap(s => (s + "\n").getBytes("utf8")).toArray)
+
+    //    val lines2 = prepareLines(List(rates14learn.last))
+    val lines2 = prepareLines(rates14learn.takeRight(100).filter(x => {
+      x(0) == 1.0
+    }))
+
+    Files.write(Paths.get("data/spy1.txt"), lines2.flatMap(s => (s + "\n").getBytes("utf8")).toArray)
+
+
+
     val spark = SparkSession
       .builder
       .appName("TestPrediction")
@@ -39,12 +77,14 @@ object TestPrediction {
 
     // $example on$
     // Load the data stored in LIBSVM format as a DataFrame.
-    val data = spark.read.format("libsvm")
+    val dataTrain = spark.read.format("libsvm")
       .load("data/spy.txt")
+    val dataTest = spark.read.format("libsvm")
+      .load("data/spy1.txt")
     // Split the data into train and test
-    val splits = data.randomSplit(Array(0.97,0.03), seed = 1234L)
-    val train = splits(0)
-    val test = splits(1)
+    //val splits = dataTrain.randomSplit(Array(0.90, 0.10), seed = 1234L)
+    val train = dataTrain
+    val test = dataTest
     // specify layers for the neural network:
     // input layer of size 4 (features), two intermediate of size 5 and 4
     // and output of size 3 (classes)
@@ -59,6 +99,10 @@ object TestPrediction {
     val model = trainer.fit(train)
     // compute accuracy on the test set
     val result = model.transform(test)
+    result.foreach(r => {
+      println("prediction:" + toStr(r.getAs("prediction")))
+    }
+    )
     val predictionAndLabels = result.select("prediction", "label")
     val evaluator = new MulticlassClassificationEvaluator()
       .setMetricName("accuracy")
@@ -67,6 +111,41 @@ object TestPrediction {
 
     spark.stop()
   }
+
+
+  def toStr(in: Double) = {
+    if (in == 0.0) "Sell" else "Buy"
+  }
+
+  def normal(set: List[Double]) = {
+    val max = set.max
+    val min = set.min
+    val middle = ((max - min) / 2.0) + min
+    set.map(rate => {
+      (((((max - rate) / (max - min) * (-1)) + 1) * 2) - 1)
+    })
+
+  }
+
+  def tail17(lines: List[String]): List[List[String]] = {
+    if (lines.size > 5 + 1)
+      List(lines.take(5 + 1)) ++ tail17(lines.tail)
+    else
+      List(lines.take(5 + 1))
+  }
+
+  def prepareLines(rates14learn: List[List[AnyVal]]) = {
+    rates14learn.map(set => {
+      val a = set.takeRight(5).map(":" + _)
+      val b = a.zip((1 to 5).toList)
+      val c = b.map(x => {
+        (x._2.toString) + x._1
+      })
+      set(0) + " " + c.mkString(" ")
+    }
+    )
+  }
+
 }
 
 // scalastyle:on println
